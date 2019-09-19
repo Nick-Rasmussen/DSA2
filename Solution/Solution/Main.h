ec1aa3270691530ffa1e38d2f35e0dc25127c06c
Nick Rasmussen (RIT Student) <nlr3026@ad.rit.edu>
2019-09-18 20:42:36 -0400

    E02 (redo)

--- Solution/Solution/Main.h
+++ Solution/Solution/Main.h
@@ -3,4 +3,16 @@
 
 #include <iostream>
 
+//Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "GL\glew.h"
+#include "GLFW\glfw3.h"
+
+#include "GLM\glm.hpp"
+
+#include "OpenGL-Tutorials\shader.hpp"
+
+
 #endif
